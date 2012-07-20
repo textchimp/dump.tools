090745d5e1d2b9aa79ef05fdffdc2a87dd986b59
Text Chimp <textchimp@gmail.com>
2012-07-20 20:07:48 +0200

    add code to handle loading JS urls from ?js=http:// querystring

--- manual-favs.js
+++ manual-favs.js
@@ -3,7 +3,8 @@ var new_manual_html = '\
 <div class="mm" style="float: left">\
   <span class="menuicon" style="background-image: url(https://confluence.atlassian.com/download/attachments/218269032/gh-icon-cog-dropdown.png); background-repeat: no-repeat; background-size: 20px 12px; width: 30px; height: 30px; position: absolute; display: none; z-index: 100; opacity: 0.7">\
   <ul class="mymenu" style="display: none; background-color: #FFF; top: 13px; position: relative; border: 1px solid #ccc; min-width: 40px; width: auto">\
-    <li onclick="javascript:showtaginput(this);"><div>tags <input type="text" style="width:40px; font-size: 10pt; display: none; border: 0px;" class="taginput"></div></li>\
+    <li onclick="javascript:shownameinput(this);"><div class="namelabel" style="font-style:italic;">longer name</div><div class="namein" style="display:none"><input type="text" style="width:80px; font-size: 10pt; border: 1px gray; color; black; margin-right: 2px;" class="nameinput" value="longer name"></div></li>\
+    <li onclick="javascript:showtaginput(this);"><div>tags </div></li>\
     <li onclick="javascript:delfav(this);">del</li>\
   </ul>\
 <div class="tagin" style="border: 1px solid #ccc; width: 140px; height: 50px; display: none; position: relative; background-color: #eee; top: 13px; left: 0px; padding: 4px;">tags:<br><input type="text" style="width:130px; font-size: 10pt; border: 1px solid #000;" class="taginput"><!--<input type="button" value="ok" onclick="javascript:add_tags($(this).prev())" class="addtagbutton">--></div>\
@@ -29,9 +30,17 @@ var new_manual_js = '\
                 }\
 		});\
 \
-		$(".taginput").blur(function(e){ if(!$(".ui-autocomplete").is(":visible") ) { $(e.target).parent().hide(); } });\
+	    $(".taginput").blur(function(e){ if(!$(".ui-autocomplete").is(":visible") ) { $(e.target).parent().hide(); $(e.target).parents("mm").children(".menuicon").hide();   } }); \
 		\
  	    $(".taginput").keypress(function(e) { if(e.charCode == 13) { add_tags(e.target); return false; } }); \
+\
+		$(".nameinput").keypress(function(e){ if(e.charCode==13){ \
+					$(this).hide();\
+					$(this).parent().prev().html($(this).val())\
+					$(this).parent().prev().show();\
+				}\
+			}).focus(function(){ $(this).select(); });\
+\
 \
         $(".mm").hover(\
              function(){ $(this).children(".menuicon").show(); },\
@@ -85,6 +94,12 @@ function manualPaletteBuildImageThumbs() {\
 }\
 \
 \
+\
+function shownameinput(e){\
+	$(e).children(".namelabel").hide(); \
+    $(e).children(".namein").show(); \
+	$(e).children(".namein").children(".nameinput").focus(); \
+}\
 function showtaginput(e) {\
  $(e).parent().hide();\
   $(e).parent().next().show();\
@@ -100,4 +115,25 @@ function add_tags(e) {\
 }\
 ';
 
-injectScriptSimple(manualPaletteBuildImageThumbs_redefine);
+/* injectScriptSimple(manualPaletteBuildImageThumbs_redefine);  */
+
+function getParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var scriptarg = getParameterByName('js');
+
+if(scriptarg.length) {
+  console.log(scriptarg);
+  if(scriptarg.indexOf("http") == 0) 
+    getScript(scriptarg, function(){});
+}
+
